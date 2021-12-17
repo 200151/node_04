a244128c3d00c157a24fa18acd2482783e680d07
200151 <200151nj@yse-c.net>
2021-12-17 09:28:09 +0900

    a

--- routes.js
+++ routes.js
@@ -1,51 +1,21 @@
 const express = require('express')
 
-const item = require('./models/item')
+const homeController = require('./controllers/HomeController')
+const loginController = require('./controllers/LoginController')
+const itemController = require('./controllers/ItemController')
+
 const router = express.Router()
 
-const default_login_name = process.env.LOGIN_NAME
-const default_passsword = process.env.PASSWORD
+//HomeController
+router.get('/', homeController.index)
+router.get('/profile', homeController.profile)
 
-router.get('/profile', (req, res) => {
-    res.render('profile/index.ejs')
-})
+//LoginController
+router.get('/login', loginController.index)
+router.post('/auth', loginController.auth)
 
-router.post('/auth', (req, res) => {
-    let message = 'ログインできません'
-    const login_name = req.body.login_name
-    const password = req.body.password
-
-    if (login_name == default_login_name
-        && password == default_passsword) {
-        message = 'ログインしました'
-    }
-    res.send(message)
-})
-
-router.get('/', (req, res) => {
-    let data = {}
-    data.title = 'トップページ'
-    res.render('index.ejs',data)
-})
-
-
-router.get('/profile', (req, res) => {
-    let user={
-        id: 1,
-        name: '畠中亮輔',
-        birthplace:'横浜',
-        hoppy:['旅行','グルメ','スポーツ'],
-    }
-    let data={}
-    data.title='プロフィール'
-    data.user=user
-})
-
-router.get('・/item/:id', (req, res) => {
-    const id=req.params.id
-    console.log(id)
-    console.log(item)
-    res.render('profile/show.ejs')
-})
+//ItemController
+router.get('/item', itemController.index)
+router.get('/item/:id', itemController.show)
 
 module.exports = router
